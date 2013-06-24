eb449849999864888bb47c79fd8e0ba901466fa0
shubhanshwy <shubhansh.varshney@webyog.com>
2013-06-24 15:27:31 +0530

    changed paths to https://agent.sealion.com

--- release/installer.sh
+++ release/installer.sh
@@ -9,5 +9,5 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-curl -s -k https://192.168.1.70/downloads/sealion.sh -o $TMP_FILE_NAME
+curl -s -k https://agent.sealion.com/sealion.sh -o $TMP_FILE_NAME
 bash $TMP_FILE_NAME $args
