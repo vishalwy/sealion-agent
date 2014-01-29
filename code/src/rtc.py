bbaa9151406d574dc25b82fe80c2bc18b06bee67
Vishal P.R <vishal.pr@webyog.com>
2014-01-29 21:16:47 +0530

    adding daemon

--- code/src/rtc.py
+++ code/src/rtc.py
@@ -1,49 +1,60 @@
 import logging
-from socketio_client import SocketIO, BaseNamespace
 import threading
+import time
+from socketio_client import SocketIO, BaseNamespace
 
 _log = logging.getLogger(__name__)
 
 class SocketIONamespace(BaseNamespace):
     def on_connect(self):
         _log.debug('Socket-io connected')
-        self.api.ping()
+        self.rtc.update_heartbeat()
+        self.rtc.api.ping()
         
     def on_disconnect(self):
         _log.debug('Socket-io disconnected')
+        
+    def on_heartbeat(self):
+        _log.debug('Socket-io heartbeat')
+        self.rtc.update_heartbeat()
 
     def on_activity_updated(self, *args):
         _log.debug('Socket-io received  activity_updated event')
-        self.api.get_config()
+        self.rtc.update_heartbeat()
+        self.rtc.api.get_config()
 
     def on_activitylist_in_category_updated(self, *args):
         _log.debug('Socket-io received  activitylist_in_category_updated event')
-        self.api.get_config()
+        self.rtc.update_heartbeat()
+        self.rtc.api.get_config()
 
     def on_agent_removed(self, *args):
         _log.debug('Socket-io received  agent_removed event')
-        self.api.stop()
+        self.rtc.api.stop()
 
     def on_org_token_resetted(self, *args):
         _log.debug('Socket-io received  org_token_resetted event')
-        self.api.stop()
+        self.rtc.api.stop()
 
     def on_server_category_changed(self, *args):
         _log.debug('Socket-io received  server_category_changed event')
-        self.api.get_config()
+        self.rtc.update_heartbeat()
+        self.rtc.api.get_config()
 
     def on_activity_deleted(self, *args):
         _log.debug('Socket-io received  activity_deleted event')
-        self.api.get_config()
+        self.rtc.update_heartbeat()
+        self.rtc.api.get_config()
         
 class Interface(threading.Thread):    
     def __init__(self, api):
         threading.Thread.__init__(self)
         self.api = api
         self.sio = None
+        self.last_heartbeat = int(time.time())
         
     def connect(self):
-        SocketIONamespace.api = self.api
+        SocketIONamespace.rtc = self
         kwargs = {
             'Namespace': SocketIONamespace,
             'cookies': self.api.cookies,
@@ -62,6 +73,12 @@ class Interface(threading.Thread):
         if self.sio != None:
             _log.debug('Disconnecting socket-io')
             self.sio.disconnect()
+            
+    def update_heartbeat(self):
+        self.last_heartbeat = int(time.time())
+        
+    def is_heartbeating(self):
+        return (int(time.time()) - self.last_heartbeat) < 30
 
     def run(self):       
         _log.debug('Starting up socket-io')
