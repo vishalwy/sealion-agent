1c1d59d94a95c56ca942648ebf00e5ead71d837c
Vishal P.R <vishal.pr@webyog.com>
2014-02-06 14:17:24 +0530

    intermediate changes

--- code/src/unregister.py
+++ code/src/unregister.py
@@ -4,8 +4,9 @@ import sys
 exe_path = os.path.dirname(os.path.abspath(__file__))
 exe_path = exe_path[:-1] if exe_path[len(exe_path) - 1] == '/' else exe_path
 exe_path = exe_path[:exe_path.rfind('/') + 1]
-sys.path.append(exe_path + 'lib')
 sys.path.append(exe_path + 'src')
+sys.path.append(exe_path + 'lib')
+sys.path.append(exe_path + 'lib/websocket_client') 
 
 from globals import Globals
 
