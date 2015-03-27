4c3dcde968c9d54470a712ea5c656205bc714227
Vishal P. R <vishal.pr@webyog.com>
2015-03-27 19:45:57 +0530

    code refactoring

--- code/bin/monit.sh
+++ code/bin/monit.sh
@@ -6,38 +6,39 @@
 #Author     : Vishal P.R
 #Email      : hello@sealion.com
 
-trap '[ $? -eq 127 ] && exit 127' ERR  #exit in case command not found
+trap '[[ $? -eq 127 ]] && exit 127' ERR  #exit in case command not found
 
-if [ "$#" != "2" ]; then
-    echo "Usage: $0 <PID> <Monit interval>"
+#get base directory of the script
+script_base_dir=$(readlink -f "$0")
+script_base_dir=${script_base_dir%/*}
+
+if [ "$#" != "2" ] ; then
+    echo "Usage: $0 <Process ID> <Monit interval>"
     exit 1
 fi
 
-#change to base directory of the script
-BASEDIR=$(readlink -f "$0")
-BASEDIR=${BASEDIR%/*}
-cd "$BASEDIR"
+cd "$script_base_dir"  #change to base directory of the script
+pid_file="../var/run/sealion.pid"  #pid file to be checked for
+service_file="../etc/init.d/sealion"  #the service script to be used for restarting agent
+log_file="../var/log/sealion.log"  #log file to be written
+orig_pid=$1  #original pid of agent
+interval=$2  #monitor interval
 
-PID_FILE="../var/run/sealion.pid"  #pid file to be checked for
-SERVICE_FILE="../etc/init.d/sealion"  #the service script to be used for restarting agent
-LOG_FILE="../var/log/sealion.log"  #log file to be written
-ORIG_PID=$1  #original pid of agent
-INTERVAL=$2  #monitor interval
-
-while true ; do
+#infinate loop
+while [[ 1 ]] ; do
     #read the pid from pid file after monitor interval
-    sleep $INTERVAL
-    PID=$(cat $PID_FILE 2>/dev/null)
+    sleep $interval
+    pid=$(cat $pid_file 2>/dev/null)
 
     #exit if pid file does not exist, or the pid is changed
-    if [[ $? -ne 0 || "$PID" != "$ORIG_PID" ]] ; then
+    if [[ $? -ne 0 || "$pid" != "$orig_pid" ]] ; then
         exit 0
     fi
 
     #check the /proc to determine if the process exist, else restart the agent
-    if [ ! -d "/proc/$PID" ] ; then
-        echo $(date +"%F %T,%3N CRITICAL ERROR - sealion was terminated; Resurrecting.") >>$LOG_FILE
-        $SERVICE_FILE start
+    if [ ! -d "/proc/$pid" ] ; then
+        echo $(date +"%F %T,%3N CRITICAL ERROR - sealion was terminated; Resurrecting.") >>$log_file
+        $service_file start
         exit 0
     fi
 done
--- code/src/execute.sh
+++ code/src/execute.sh
@@ -6,54 +6,49 @@
 #Author     : Vishal P.R
 #Email      : hello@sealion.com
 
-terminate()
-{
-    #Function to close the output streams and kill background jobs.
+#trap exit, and send signal to sub-shell jobs, which in turn kills their children
+trap "terminate" EXIT
 
+#Function to close the output streams and kill background jobs.
+terminate() {
     exec 1>&-  #close stdout
     exec 2>&-  #close stderr
     kill -SIGTERM $(jobs -p) >/dev/null 2>&1
 }
 
-#trap exit, and send signal to sub-shell jobs, which in turn kills their children
-trap "terminate" EXIT
+#Function to kill children
+kill_children() {
+    if [ $no_setsid -eq 0 ] ; then  #kill process group if we have setsid
+        kill -SIGKILL -- -$SESSION_PID  >/dev/null 2>&1
+    else 
+        kill -SIGKILL $SESSION_PID >/dev/null 2>&1  #bad luck; the grand children are still alive
+    fi
+}
 
 #initialize the indexes of each column in the line read from stdin
-TIMESTAMP=0  #unique timestamp of the activity
-OUTPUT=1  #filename of output
-COMMAND=2  #command to be executed, this has to be the last index as a command can have spaces in it
+timestamp_index=0  #unique timestamp of the activity
+output_index=1  #filename of output
+command_index=2  #command to be executed, this has to be the last index as a command can have spaces in it
 
-PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin  #common paths found in various linux distros
+PATH="${PATH}:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"  #common paths found in various linux distros
 
 #check whether we have setsid available
 type setsid >/dev/null 2>&1
-NO_SETSID=$?
+no_setsid=$?
 
 #if setsid is not available we wont be able to run the activities in a new session
 #which means, we wont be able to kill the process tree if the command timeout
-if [ $NO_SETSID -ne 0  ] ; then
-    echo "warning: Cannot run commands as process group; setsid not available"
-fi
+[[ $no_setsid -ne 0 ]] && echo "warning: Cannot run commands as process group; setsid not available"
 
 #continuously read line from stdin, blocking read.
 #format of a line is 'TIMESTAMP OUTPUT_FILE: COMMAND_WITH_SPACES'
-while read -r LINE ; do
-    ACTIVITY=(${LINE%%:*})  #make activity array from string upto ':' character
-    ACTIVITY=("${ACTIVITY[@]}" "${LINE#*:}")  #add string after ':' character to activity array
+while read -r line ; do
+    activity=(${line%%:*})  #make activity array from string upto ':' character
+    activity=("${activity[@]}" "${line#*:}")  #add string after ':' character to activity array
 
     (
-        #This is a sub-shell, which is forked from parent process. We run this as a background job to enable parallel execution.
-        
-        kill_children()
-        {
-            #Function to kill children.
-            
-            if [ $NO_SETSID -eq 0 ] ; then  #kill process group if we have setsid
-                kill -SIGKILL -- -$SESSION_PID  >/dev/null 2>&1
-            else 
-                kill -SIGKILL $SESSION_PID >/dev/null 2>&1
-            fi
-        }
+        #This is a sub-shell, which is forked from parent process. 
+        #We run this as a background job to enable parallel execution.
 
         trap "kill_children" SIGTERM  #kill children on exit
 
@@ -63,16 +58,16 @@ while read -r LINE ; do
             BASHPID=${BASHPID[4]}  #pid is at the 4th index
         fi
 
-        echo "data: ${ACTIVITY[$TIMESTAMP]} pid $BASHPID"  #write out the process id for tracking purpose
+        echo "data: ${activity[$timestamp_index]} pid $BASHPID"  #write out the process id for tracking purpose
 
-        if [ $NO_SETSID -eq 0 ] ; then  #run it in a new session
-            setsid bash -c "${ACTIVITY[$COMMAND]}" 1>"${ACTIVITY[$OUTPUT]}" 2>"${ACTIVITY[$OUTPUT]}" &
+        if [ $no_setsid -eq 0 ] ; then  #run it in a new session
+            setsid bash -c "${activity[$command_index]}" 1>"${activity[$output_index]}" 2>"${activity[$output_index]}" &
         else
-            bash -c "${ACTIVITY[$COMMAND]}" 1>"${ACTIVITY[$OUTPUT]}" 2>"${ACTIVITY[$OUTPUT]}" &
+            bash -c "${activity[$command_index]}" 1>"${activity[$output_index]}" 2>"${activity[$output_index]}" &
         fi
 
         SESSION_PID=$!  #pid of the bash process
         wait  #wait for the background job to finish
-        echo "data: ${ACTIVITY[$TIMESTAMP]} return_code $?"  #write out the return code which indicates that the process has finished
+        echo "data: ${activity[$timestamp_index]} return_code $?"  #write out the return code which indicates that the process has finished
     ) &
 done
